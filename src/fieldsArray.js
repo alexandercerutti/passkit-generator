b35880e2bb90a138a075ba8f34b2172cdbd0674d
Ian Bale <ian@hazardousfrog.com>
2019-06-04 10:49:28 +0100

    changes space indents to tabs

--- src/fieldsArray.js
+++ src/fieldsArray.js
@@ -2,102 +2,100 @@ const schema = require("./schema");
 const debug = require("debug")("passkit:fields");
 
 /**
+ * Pass fields must be unique (for key) in its scope.
+ * Therefore we use a Set to keep them tracked.
+ */
+
+/**
  * Class to represent lower-level keys pass fields
  * @see https://apple.co/2wkUBdh
  */
 
-class FieldsArray extends Set {
-  constructor(fields) {
-    super();
+class FieldsArray extends Object {
+	constructor(fields) {
+		super();
 
-    /**
-     * Pass fields must be unique (for key) in its scope.
-     * Therefore we use a Set to keep them tracked.
-     */
+		/**
+		 * Pass fields must be unique (for key) in its scope.
+		 * Therefore we use a Set to keep them tracked.
+		*/
 
-    this.fieldsKeys = new Set();
-    fields.forEach(a => this[a] = new ItemsArray(this));
-  }
+		this.fieldsKeys = new Set();
+		fields.forEach(a => this[a] = new ItemsArray(this));
+	}
 
-  addFieldKey(key) {
+	addFieldKey(key) {
+		if (this.fieldsKeys.has(key)) {
+			debug(`Field with key "${key}" discarded: fields must be unique in pass scope.`);
+		} else {
+			this.fieldsKeys.add(key);
+		}
+	}
 
-    if (this.fieldsKeys.has(key)) {
-      debug(`Field with key "${key}" discarded: fields must be unique in pass scope.`);
-    } else {
-      this.fieldsKeys.add(key);
-    }
-  }
+	deleteFieldKey(key) {
+		this.fieldsKeys.delete(key);
+	}
 
-  deleteFieldKey(key) {
-    this.fieldsKeys.delete(key);
-  }
-
-  emptyUnique() {
-    this.fieldsKeys.clear();
-  }
+	emptyUnique() {
+		this.fieldsKeys.clear();
+	}
 }
-
 
 class ItemsArray extends Array {
-  constructor(owner) {
-    super();
+	constructor(owner) {
+		super();
+	}
 
-    this.owner = owner;
-  }
+	/**
+	 * Like `Array.prototype.push` but will alter
+	 * also uniqueKeys set.
+	 */
 
-  /**
-   * Like `Array.prototype.push` but will alter
-   * also uniqueKeys set.
-   */
+	push(...fieldsData) {
+		const validFields = fieldsData.reduce((acc, current) => {
+			if (!(typeof current === "object") || !schema.isValid(current, "field")) {
+				return acc;
+			}
 
-  push(...fieldsData) {
-    const validFields = fieldsData.reduce((acc, current) => {
-      if (!(typeof current === "object") || !schema.isValid(current, "field")) {
-        return acc;
-      }
+			if (acc.some(e => e.key === current.key) || fieldsKeys.has(current.key)) {
+				debug(`Field with key "${current.key}" discarded: fields must be unique in pass scope.`);
+			} else {
+				fieldsKeys.add(current.key);
+				acc.push(current);
+			}
 
-      if (acc.some(e => e.key === current.key)) {
-        debug(`Field with key "${key}" discarded: fields must be unique in pass scope.`);
-      } 
+			return acc;
+		}, []);
 
-      this.owner.addFieldKey(current.key)
-      acc.push(current)
+		return Array.prototype.push.call(this, ...validFields);
+	}
 
-      return acc;
-    }, []);
+	/**
+	 * Like `Array.prototype.pop`, but will alter
+	 * also uniqueKeys set
+	 */
 
-    return Array.prototype.push.call(this, ...validFields);
-  }
+	pop() {
+		const element = Array.prototype.pop.call(this);
+		fieldsKeys.delete(element.key);
+		return element;
+	}
 
-  /**
-   * Like `Array.prototype.pop`, but will alter
-   * also uniqueKeys set
-   */
+	/**
+	 * Like `Array.prototype.splice` but will alter
+	 * also uniqueKeys set
+	 */
 
-  pop() {
-    const element = Array.prototype.pop.call(this);
+	splice(start, deleteCount, ...items) {
+		const removeList = this.slice(start, deleteCount+start);
+		removeList.forEach(item => fieldsKeys.delete(item.key));
 
-    this.owner.deleteFieldKey(element.key)
-    return element;
-  }
+		return Array.prototype.splice.call(this, start, deleteCount, items);
+	}
 
-  /**
-   * Like `Array.prototype.splice` but will alter
-   * also uniqueKeys set
-   */
-
-  splice(start, deleteCount, ...items) {
-    const removeList = this.slice(start, deleteCount+start);
-
-    removeList.forEach(item => this.owner.deleteFieldKey(item.key));
-
-    return Array.prototype.splice.call(this, start, deleteCount, items);
-  }
-
-  get length() {
-    return this.length;
-  }
+	get length() {
+		return this.length;
+	}
 }
-
 
 module.exports = FieldsArray;

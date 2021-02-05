28780ebc72705a6f4cede3a5ca20de3314657be4
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-02-05 08:11:27 +0100

    Moved main index under src folder

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Pass as PassClass } from "./pass";
+import { AbstractModel as AbstractModelClass } from "./abstract";
+
+export { createPass } from "./factory";
+export { createAbstractModel } from "./abstract";
+export type Pass = InstanceType<typeof PassClass>
+export type AbstractModel = InstanceType<typeof AbstractModelClass>

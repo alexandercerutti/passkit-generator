4470da11ca7d67c4e5a41605d1b6f90bea45dd22
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-08-25 22:44:59 +0200

    Added support to requiresAuthentication for NFC schema and interface, along with developer reference link

--- src/schemas/nfc.ts
+++ src/schemas/nfc.ts
@@ -1,11 +1,17 @@
 import Joi from "joi";
 
+/**
+ * @see https://developer.apple.com/documentation/walletpasses/pass/nfc
+ */
+
 export interface NFC {
 	message: string;
-	encryptionPublicKey?: string;
+	encryptionPublicKey: string;
+	requiresAuthentication?: boolean;
 }
 
 export const NFC = Joi.object<NFC>().keys({
 	message: Joi.string().required().max(64),
-	encryptionPublicKey: Joi.string(),
+	encryptionPublicKey: Joi.string().required(),
+	requiresAuthentication: Joi.boolean(),
 });

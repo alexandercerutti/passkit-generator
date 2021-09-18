a97023262374652141895d00fc38282df17c6896
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-09-18 21:45:46 +0200

    Added fields and pool creation to PKPass

--- src/PKPass.ts
+++ src/PKPass.ts
@@ -1,11 +1,21 @@
-import { Certificates } from "../lib/schemas";
+import FieldsArray from "../src/fieldsArray";
+import { Certificates } from "../src/schemas";
 import { default as Bundle, filesSymbol } from "./Bundle";
+
+const fieldKeysPoolSymbol = Symbol("fieldKeysPoolSymbol");
 
 interface NamedBuffers {
 	[key: string]: Buffer;
 }
 
 export class PKPass extends Bundle {
+	private [fieldKeysPoolSymbol] = new Set<string>();
+	public primaryFields /*****/ = new FieldsArray(this[fieldKeysPoolSymbol]);
+	public secondaryFields /***/ = new FieldsArray(this[fieldKeysPoolSymbol]);
+	public auxiliaryFields /***/ = new FieldsArray(this[fieldKeysPoolSymbol]);
+	public headerFields /******/ = new FieldsArray(this[fieldKeysPoolSymbol]);
+	public backFields /********/ = new FieldsArray(this[fieldKeysPoolSymbol]);
+
 	constructor(buffers: NamedBuffers, certificates: Certificates) {
 		super("application/vnd.apple.pkpass");
 

e8a40c45545e984312c2f5d8eec0d4195e009a38
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-09-27 23:48:25 +0200

    Deleted factory.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export type { Pass } from "./pass";
 export type { AbstractModel } from "./abstract";
 
-export { createPass } from "./factory";
 export { createAbstractModel } from "./abstract";
 
 export { default as Bundle } from "./Bundle";

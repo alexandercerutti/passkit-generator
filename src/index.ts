3d22cef8054bf65d38ab7d2e424b986da71555e6
Alexander Cerutti <cerutti.alexander@gmail.com>
2021-10-03 22:14:33 +0200

    Removed Abstract Model implementation and added PKPass as esport

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-export type { Pass } from "./pass";
-export type { AbstractModel } from "./abstract";
-
-export { createAbstractModel } from "./abstract";
-
 export { default as Bundle } from "./Bundle";
+export { default as PKPass } from "./PKPass";

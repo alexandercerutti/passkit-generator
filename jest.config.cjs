c645a3fb141b27fb1135f77634ce47d6131c2f9b
Alexander Cerutti <cerutti.alexander@gmail.com>
2023-04-04 21:05:25 +0200

    Added settings for jest

---
+++ jest.config.cjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import("jest").Config}
+ */
+
+module.exports = {
+	moduleFileExtensions: ["js", "mjs", "cjs"],
+	testEnvironment: "node",
+	testMatch: ["**/specs/**/*.spec.cjs"],
+};

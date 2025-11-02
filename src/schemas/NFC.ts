cdfb80b53b69531706188d67dd7c8edbf0720ccd
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-11-02 15:47:06 +0100

    Converted NFC schema to zod

--- src/schemas/NFC.ts
+++ src/schemas/NFC.ts
@@ -1,17 +1,12 @@
-import Joi from "joi";
+import { z } from "zod";
 
 /**
  * @see https://developer.apple.com/documentation/walletpasses/pass/nfc
  */
+export type NFC = z.infer<typeof NFC>;
 
-export interface NFC {
-	message: string;
-	encryptionPublicKey: string;
-	requiresAuthentication?: boolean;
-}
-
-export const NFC = Joi.object<NFC>().keys({
-	message: Joi.string().required().max(64),
-	encryptionPublicKey: Joi.string().required(),
-	requiresAuthentication: Joi.boolean(),
+export const NFC = z.object({
+	message: z.string().check(z.maxLength(64)),
+	encryptionPublicKey: z.string(),
+	requiresAuthentication: z.boolean().optional(),
 });

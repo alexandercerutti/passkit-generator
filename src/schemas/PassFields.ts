dd8eb271b380f4ea45120461956e613c5d39c7f4
Alexander Cerutti <cerutti.alexander@gmail.com>
2025-11-13 00:31:28 +0100

    Made PassFields partial

--- src/schemas/PassFields.ts
+++ src/schemas/PassFields.ts
@@ -16,20 +16,22 @@ export const TransitType = z.literal([
 
 export type PassFields = z.infer<typeof PassFields>;
 
-export const PassFields = z.object({
-	auxiliaryFields: z.array(PassFieldContentWithRow.or(PassFieldContent)),
-	backFields: z.array(PassFieldContent),
-	headerFields: z.array(PassFieldContent),
-	primaryFields: z.array(PassFieldContent),
-	secondaryFields: z.array(PassFieldContent),
-	transitType: TransitType.optional(),
+export const PassFields = z
+	.object({
+		auxiliaryFields: z.array(PassFieldContentWithRow.or(PassFieldContent)),
+		backFields: z.array(PassFieldContent),
+		headerFields: z.array(PassFieldContent),
+		primaryFields: z.array(PassFieldContent),
+		secondaryFields: z.array(PassFieldContent),
+		transitType: TransitType,
 
-	/**
-	 * @iOSVersion 18
-	 * @passStyle eventTicket (new layout)
-	 * @passDomain dashboard
-	 *
-	 * @see \<undiclosed>
-	 */
-	additionalInfoFields: z.array(PassFieldContent),
-});
+		/**
+		 * @iOSVersion 18
+		 * @passStyle eventTicket (new layout)
+		 * @passDomain dashboard
+		 *
+		 * @see \<undiclosed>
+		 */
+		additionalInfoFields: z.array(PassFieldContent),
+	})
+	.partial();
